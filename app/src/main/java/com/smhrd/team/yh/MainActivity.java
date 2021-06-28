4fbf1e5ea82002298747cb356bfb76d7bcd5d548
smhrd <yh960122@naver.com>
2021-06-28 11:06:58 +0900

    로그 추가~!

--- app/src/main/java/com/smhrd/team/yh/MainActivity.java
+++ app/src/main/java/com/smhrd/team/yh/MainActivity.java
@@ -3,6 +3,7 @@ package com.smhrd.team.yh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("kgh","111");
 
     }
 }
