e068b9520284ce2bdb1f284d455f38f4c15ab5d8
전유이 <jeonyui1004@icloud.com>
2021-07-08 17:57:50 +0900

    2021-07-08-yui-last join1페이지 완료 join2페이지 id찾기 pw찾기 틀만 만듬

--- app/src/main/java/com/smhrd/team/yh/Fragment_C.java
+++ app/src/main/java/com/smhrd/team/yh/Fragment_C.java
@@ -7,15 +7,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Fragment_C extends Fragment {
-    private Button fragc_btn_all, fragc_btn_choice;
+
+    private Button fragc_btn_all, fragc_btn_choice, btn_happyham;
+    private ImageView imghappyham;
+    private TextView mainview, happytitle, happysub;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View fragment=inflater.inflate(R.layout.fragment_c,container,false);
 
-//
+        imghappyham = fragment.findViewById(R.id.imghappyham);
+        btn_happyham = fragment.findViewById(R.id.btn_happyham);
+        mainview = fragment.findViewById(R.id.mainview);
+        happytitle = fragment.findViewById(R.id.happytitle);
+        happysub = fragment.findViewById(R.id.happysub);
         fragc_btn_all = fragment.findViewById(R.id.fragc_btn_all);
         fragc_btn_choice = fragment.findViewById(R.id.fragc_btn_choice);
 
