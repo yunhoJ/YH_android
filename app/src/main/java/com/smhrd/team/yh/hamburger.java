d50c2cfac273031d2f587bf6ab68064f336f7b0e
전유이 <jeonyui1004@icloud.com>
2021-07-08 11:19:31 +0900

    2021-07-08-yui-1119am

---
+++ app/src/main/java/com/smhrd/team/yh/hamburger.java
@@ -0,0 +1,14 @@
+package com.smhrd.team.yh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hamburger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hamburger);
+    }
+}
